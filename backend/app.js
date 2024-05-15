459473fc8033ce83b274cbc34a5fdd586fda8b3a
Amlend <amlendsingh@gmail.com>
2024-05-16 02:38:49 +0530

    initial-backend-setup

---
+++ backend/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+require("dotenv").config();
+const bodyParser = require("body-parser");
+const path = require("path");
+const sequelize = require("./util/database");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+sequelize
+  .sync()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is Running on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
