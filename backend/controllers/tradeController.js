1553e0fd5b9f738e655489ae112ee6ee4bb6fae6
Amlend <amlendsingh@gmail.com>
2024-05-16 03:54:09 +0530

    added-get-trade-by-id-and-other-API

--- backend/controllers/tradeController.js
+++ backend/controllers/tradeController.js
@@ -34,3 +34,31 @@ exports.getTrade = async (req, res, next) => {
     res.status(500).json({ message: "Error fetching trades" });
   }
 };
+
+exports.getTradeById = async (req, res, next) => {
+  const id = parseInt(req.params.id);
+
+  try {
+    const trade = await Trade.findByPk(id); // Find trade by primary key
+    if (trade) {
+      res.status(200).json(trade);
+    } else {
+      res.status(404).send("ID not found");
+    }
+  } catch (error) {
+    console.error("Error fetching trade:", error.message);
+    res.status(500).json({ message: "Error fetching trade" });
+  }
+};
+
+exports.deleteTrade = (req, res, next) => {
+  res.status(405).send("Method Not Allowed");
+};
+
+exports.putTrade = (req, res, next) => {
+  res.status(405).send("Method Not Allowed");
+};
+
+exports.patchTrade = (req, res, next) => {
+  res.status(405).send("Method Not Allowed");
+};
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -3,6 +3,10 @@ const tradeController = require("../controllers/tradeController");
 const router = express.Router();
 
 router.post("/trades", tradeController.postTrade);
+router.get("/trades/:id", tradeController.getTradeById);
 router.get("/trades", tradeController.getTrade);
+router.delete("/trades/:id", tradeController.deleteTrade);
+router.put("/trades/:id", tradeController.putTrade);
+router.patch("/trades/:id", tradeController.patchTrade);
 
 module.exports = router;
